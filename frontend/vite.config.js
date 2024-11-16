49a965b72a50651352980b0df083b0e101ef8804
Utkarsh Palav <167518906+Utkarsh1714@users.noreply.github.com>
2024-11-17 03:00:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://social-media-app-mern-app-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
