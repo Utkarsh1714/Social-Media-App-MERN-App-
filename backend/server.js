a732d597933e0719bb9e55c72561fbf7920e0b48
Utkarsh1714 <utkarshpalav17@gmail.com>
2024-11-17 03:56:28 +0530

    SERVER.JS

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(
   cors({
     origin: "https://social-media-app-mern-app-frontend.onrender.com", // Frontend URL
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
